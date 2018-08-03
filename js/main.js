ab617830186f2c0a84a60037f2f44222472a537f
James <jamesakpan016@gmail.com>
2018-08-03 02:11:52 +0100

    made the menu bar better

--- js/main.js
+++ js/main.js
@@ -5,15 +5,3 @@ const menuOpener = document.querySelector(".mainHeader > i");
 
 
 
-
-menuOpener.addEventListener("click", () =>
-{
-    mainshow.style.display = "none";
-    menu.style.display = "block";
-})
-
-cancel.addEventListener("click", () =>
-{
-    menu.style.display = "none";
-    mainshow.style.display = "initial";
-})
