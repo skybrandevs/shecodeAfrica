99eca7fd2d5814a8791b23a7183f84d81bb9b2de
James <jamesakpan016@gmail.com>
2018-08-04 21:43:52 +0100

    changes made on 8/4/18

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 const mainshow = document.querySelector(".mainshow")
-const menu = document.querySelector(".menu");
-const cancel= document.querySelector(".cancel");
+const menu = document.querySelector(".menucontainer");
+const cancel= document.querySelector(".menuout");
+
 const menuOpener = document.querySelector(".mainHeader > i");
 
+menuOpener.addEventListener("click", () =>
+{
+    menu.style.display = "block";
+    menu.style.animation = "sliderightmenu 1.3s"
+})
 
 
+cancel.addEventListener("click", () =>
+{
+   
+    menu.style.animation = "sliderightmenu 1.3s";
+    menu.style.display = "none";
+
+})
